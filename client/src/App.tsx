c1aea6ad79659b645232b3a2e663804c91bc3132
LisaBlas <bleepbloopproducts@gmail.com>
2025-05-16 17:16:18 +0200

    Remove unused components

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,11 @@
-import { Switch, Route } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
+import { Route, Switch } from "wouter";
+import { Toaster } from "./components/ui/toaster";
+import Home from "./pages/home";
+import { FilterProvider } from "./context/FilterContext";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import NotFound from "@/pages/not-found";
-import Home from "@/pages/home";
-import { FilterProvider } from "./context/FilterContext";
 import './App.css';
 
 function Router() {
@@ -18,7 +18,7 @@ function Router() {
   );
 }
 
-function App() {
+export default function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
@@ -32,5 +32,3 @@ function App() {
     </QueryClientProvider>
   );
 }
-
-export default App;
