602e5ae6aa4fbbaf64961f87de07797ef1d5c47f
LisaBlas <bleepbloopproducts@gmail.com>
2025-05-19 22:10:04 +0200

    More Meta tags and h1 content modifications

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -4,13 +4,13 @@ const Hero: React.FC = () => {
   return (
     <div className="text-center py-40">
       <h1 className="text-4xl md:text-7xl font-bold text-foreground mb-6 leading-[1.4]">
-        Find Your Next<br />
+          Use unique keywords. <br />
         <span className="inline-block bg-gradient-to-r from-primary to-white bg-clip-text text-transparent">
-          Favourite Game
+          Find Better Games.
         </span>
       </h1>
       <p className="text-lg md:text-xl text-muted-foreground max-w-3xl mx-auto px-4 leading-[1.8]">
-          Hyper-targeted game recommendations<br /> powered by unique filters.
+          Hyper-targeted game recommendations<br /> powered by gamer-crafted filters.
       </p>
     </div>
   );
