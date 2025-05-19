c54db324b602a688a5c4885dc5145b9c398cdea9
LisaBlas <bleepbloopproducts@gmail.com>
2025-05-19 21:11:21 +0200

    SEO files and meta-tags set

--- server/vite.ts
+++ server/vite.ts
@@ -77,6 +77,15 @@ export function serveStatic(app: Express) {
   }
 
   app.use(express.static(distPath));
+  
+   // Add explicit routes for robots.txt and sitemap.xml
+   app.get('/robots.txt', (req, res) => {
+    res.sendFile(path.resolve(distPath, 'robots.txt'));
+  });
+
+  app.get('/sitemap.xml', (req, res) => {
+    res.sendFile(path.resolve(distPath, 'sitemap.xml'));
+  });
 
   // fall through to index.html if the file doesn't exist
   app.use("*", (_req, res) => {
