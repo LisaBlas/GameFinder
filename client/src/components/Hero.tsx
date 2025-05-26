f641d1b467dfc2340a5ccc36f95a08d8fd087cdf
LisaBlas <bleepbloopproducts@gmail.com>
2025-05-26 15:18:24 +0200

    Changed Headline

--- client/src/components/Hero.tsx
+++ client/src/components/Hero.tsx
@@ -10,9 +10,9 @@ const Hero: React.FC = () => {
   return (
     <div className="hero-section text-center">
       <h1 className="text-4xl md:text-7xl font-bold text-foreground mb-6 leading-[1.4]">
-          Use unique keywords. <br />
+          Find your next <br />
         <span className="inline-block bg-gradient-to-r from-primary to-white bg-clip-text text-transparent">
-          Find Better Games.
+          Favourite Game.
         </span>
       </h1>
       <h2 className="text-lg md:text-xl text-muted-foreground max-w-3xl mx-auto px-4 leading-[1.8]">
